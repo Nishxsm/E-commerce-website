20d6d595c3aa8b2add7f536e16759872647d2525
Nishxsm <nishantdandnaik156@gmail.com>
2025-01-25 17:32:45 +0530

    updated frontend

--- js scripts/script.js
+++ js scripts/script.js
@@ -1,3 +1,4 @@
+//to toggle sections 
 const links = document.querySelectorAll('.nav-left .nav-item');
 const sections = document.querySelectorAll('section');
 
@@ -16,3 +17,26 @@ links.forEach(link => {
         }
     });
 });
+
+
+//to toggle burger menu
+
+function toggleMenu() {
+    const sideMenu = document.getElementById('sideMenu');
+    const sideMenuActive = sideMenu.classList.contains('active');
+    
+    if (sideMenuActive) {
+        sideMenu.classList.remove('active');
+    } else {
+        sideMenu.classList.add('active');
+    }
+    
+    // Close menu when clicking anywhere outside of it
+    document.addEventListener('click', function(event) {
+        if (!sideMenu.contains(event.target) && !document.querySelector('.burger-icon').contains(event.target)) {
+            sideMenu.classList.remove('active');
+        }
+    });
+}
+
+  
